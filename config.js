c49c05fe372b9f2ad861bc0d2298a6ba0a3e97b0
Ernisss <34385067+Ernisss@users.noreply.github.com>
2020-03-29 13:15:28 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@ exports.TOKEN = 'Mzg4ODAzNjMzNzE1ODcxNzU0.XoBwXw.eDSVSfa_Puld-e6_5f5dzX58QeQ';
 
 exports.PREFIX = '#';
 
-exports.GOOGLE_API_KEY = 'AIzaSyDiQJe3qNoO8YC9VNU5VH9WIvxXh3TnlrY';
+exports.GOOGLE_API_KEY = 'AIzaSyBBxbBF9MFIl0tZhLlbKqX5uj_d9XCs2zU';
